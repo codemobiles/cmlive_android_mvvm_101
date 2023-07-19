03198e3deddd58ff824fa20ee928abc14ace9c38
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-19 21:02:42 +0700

    1234

--- cmmvvm101/app/src/main/java/com/codemobiles/cmmvvm101/viewmodel/MainViewModel.kt
+++ cmmvvm101/app/src/main/java/com/codemobiles/cmmvvm101/viewmodel/MainViewModel.kt
@@ -1,6 +1,8 @@
 package com.codemobiles.cmmvvm101.viewmodel
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class MainViewModel: ViewModel() {
+    val count = MutableStateFlow<Int>(0)
 }
