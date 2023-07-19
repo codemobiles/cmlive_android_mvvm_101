b5697eab696aec99c8b6c2cd694e2f1c6c20f6ad
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-19 21:09:42 +0700

    1234

--- cmmvvm101/app/src/main/java/com/codemobiles/cmmvvm101/MainActivity.kt
+++ cmmvvm101/app/src/main/java/com/codemobiles/cmmvvm101/MainActivity.kt
@@ -4,26 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.codemobiles.cmmvvm101.databinding.ActivityMainBinding
+import com.codemobiles.cmmvvm101.viewmodel.AppViewModelFactory
+import com.codemobiles.cmmvvm101.viewmodel.MainViewModel
 
 //  V--MV--M
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
-    private var count = 0
+    private lateinit var viewModel:MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val factory = AppViewModelFactory(context = applicationContext)
+        viewModel = ViewModelProvider(this, factory).get(MainViewModel::class.java)
 
-        binding.countBtn.text = "Count $count"
+
+        binding.countBtn.text = "Count ${viewModel.count.value}"
         setUIEvent()
     }
 
     private fun setUIEvent() {
         binding.countBtn.setOnClickListener {
-            count++
-            binding.countBtn.text = "Count $count"
+            viewModel.count.value = viewModel.count.value + 1
+            binding.countBtn.text = "Count ${viewModel.count.value}"
         }
     }
 }
--- cmmvvm101/app/src/main/java/com/codemobiles/cmmvvm101/viewmodel/MainViewModel.kt
+++ cmmvvm101/app/src/main/java/com/codemobiles/cmmvvm101/viewmodel/MainViewModel.kt
@@ -1,8 +1,9 @@
 package com.codemobiles.cmmvvm101.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 
-class MainViewModel: ViewModel() {
+class MainViewModel(val context: Context): ViewModel() {
     val count = MutableStateFlow<Int>(0)
 }
