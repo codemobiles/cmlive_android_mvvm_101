24585dcd99fdaeb12282d1ece31d655f16aed6e4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-19 20:47:32 +0700

    1234

--- cmmvvm101/app/src/main/java/com/codemobiles/cmmvvm101/MainActivity.kt
+++ cmmvvm101/app/src/main/java/com/codemobiles/cmmvvm101/MainActivity.kt
@@ -2,10 +2,21 @@ package com.codemobiles.cmmvvm101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUIEvent()
+    }
+
+    private fun setUIEvent() {
+        val countBtn = findViewById<Button>(R.id.countBtn)
+        countBtn.setOnClickListener {
+            Toast.makeText(applicationContext, "Hey", Toast.LENGTH_SHORT).show()
+        }
     }
 }
