0c79416e73321ae43092faee21e4be21950e60ad
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-19 20:45:01 +0700

    1234

---
+++ cmmvvm101/app/src/main/java/com/codemobiles/cmmvvm101/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codemobiles.cmmvvm101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
