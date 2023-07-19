6f9b28167677b5ff1f398a1486cee15412dd450b
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-19 20:58:38 +0700

    1234

--- cmmvvm101/app/src/main/java/com/codemobiles/cmmvvm101/MainActivity.kt
+++ cmmvvm101/app/src/main/java/com/codemobiles/cmmvvm101/MainActivity.kt
@@ -6,19 +6,24 @@ import android.widget.Button
 import android.widget.Toast
 import com.codemobiles.cmmvvm101.databinding.ActivityMainBinding
 
+//  V--MV--M
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.countBtn.text = "Count $count"
         setUIEvent()
     }
 
     private fun setUIEvent() {
         binding.countBtn.setOnClickListener {
-            Toast.makeText(applicationContext, "Hey", Toast.LENGTH_SHORT).show()
+            count++
+            binding.countBtn.text = "Count $count"
         }
     }
 }
---
+++ cmmvvm101/app/src/main/java/com/codemobiles/cmmvvm101/viewmodel/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.codemobiles.cmmvvm101.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+}
